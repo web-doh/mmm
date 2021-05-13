84c320ae9d3199e6b78ab75a5a4fe4de8d47de3b
web-doh <66818228+web-doh@users.noreply.github.com>
2021-05-13 17:09:45 +0900

    [F-Fix] 아이템 save할 때 이미지로 저장

--- src/client/components/button/save_button.jsx
+++ src/client/components/button/save_button.jsx
@@ -1,47 +1,40 @@
-import React, { memo, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
 import styles from "./save_button.module.css";
 import { Download } from "react-feather";
 
-const getRelativeSize = (childId, parent) => {
-  const child = document
-    .querySelector(`#img${childId}`)
-    .getBoundingClientRect();
+const modifySVG = () => {
+  const parent = document.querySelector(`#modal`);
+  const svg = parent.querySelectorAll("svg");
 
-  const width = Math.floor(child.width * 0.2645833); // px to mm
-  const height = Math.floor(child.height * 0.2645833);
-  const y = Math.floor((child.y - parent.y) * 0.2645833);
-  const x = Math.floor((child.x - parent.x) * 0.2645833);
-
-  return { x, y, width, height };
+  svg.forEach((item) => {
+    item.setAttribute("width", item.getBoundingClientRect().width);
+    item.setAttribute("height", item.getBoundingClientRect().height);
+    item.style.width = null;
+    item.style.height = null;
+  });
 };
 
 const SaveButton = memo(({ name, tagId, imgs }) => {
+  useEffect(() => modifySVG(), []);
+
   const saveToPdf = () => {
     const screenshot = document.querySelector(`#${tagId}`);
     const size = screenshot.getBoundingClientRect();
 
-    html2canvas(screenshot).then((canvas) => {
+    html2canvas(screenshot, {
+      letterRendering: 1,
+      useCORS: true,
+      height: size.height + 40,
+      width: size.width + 40,
+      x: size.x - 20,
+      y: window.scrollY + size.y - 20,
+    }).then((canvas) => {
       const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF("p", "mm", "a4");
-      pdf.addImage(imgData, "PNG", 10, 10);
-      for (let i = 0; i < 3; i += 1) {
-        if (!imgs[i]["url"]) continue;
-        const imgSize = getRelativeSize(i, size);
-        const { x, y, width, height } = imgSize;
-
-        pdf.addImage(
-          imgs[i]["url"],
-          "PNG",
-          x + 11,
-          y + 10.2,
-          width * 0.99,
-          height * 0.99
-        );
-      }
-
-      pdf.save(`${name}.pdf`);
+      const el = document.createElement("a");
+      el.href = imgData;
+      el.download = `${name}.png`;
+      el.click();
     });
   };
 
