2253493f1b370cba2abaff6a922a71ee0139c90b
web-doh <cocov2222@gmail.com>
2021-12-08 16:42:11 +0900

    [F-Refactor] add srcSet to logo image

--- src/client/pages/home/home.jsx
+++ src/client/pages/home/home.jsx
@@ -1,15 +1,20 @@
-import React, { memo, useEffect, useState } from "react";
+import React, { memo } from "react";
 import styles from "./home.module.css";
 import logo from "../../assets/logo.png";
+import smallLogo from "../../assets/logo2.png";
 import LinkButton from "../../components/button/link_button";
 import IntroCards from "../../components/intro_cards/intro_cards";
-import { Link } from "react-router-dom";
 
 const Home = memo(() => {
   return (
     <div className={styles.containers}>
       <section className={`${styles.container} ${styles.left}`}>
-        <img src={logo} className={styles.logo} alt="mmm logo" />
+        <img
+          src={smallLogo}
+          srcSet={`${smallLogo} 200w, ${logo} 240w`}
+          className={styles.logo}
+          alt="mmm logo"
+        />
         <h3 className={styles.title}>
           <b>M</b>y <b>M</b>aterials <b>M</b>anager
         </h3>
